92c14b227f6af9448bd41dee222b0c73e405378b
AbednegoTenge <abednegotenge180@gmail.com>
2026-04-17 17:57:19 +0000

    configuring celery

---
+++ config/celery.py
@@ -0,0 +1,17 @@
+import os
+
+from celery import Celery
+from celery.schedules import crontab
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery('lms')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'close-expired-quizzes-every-5-minutes': {
+        'task': 'apps.assessments.tasks.close_expired_quizzes',
+        'schedule': crontab(minute='*/5'),
+    },
+}
